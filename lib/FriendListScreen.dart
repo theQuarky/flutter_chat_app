3bfca93dae2ef150e8b007ecd72a9ffd01577c80
Hiren <ranahiren27@gmail.com>
2024-07-15 03:24:23 +0530

    fixing duplicate name bug

--- lib/FriendListScreen.dart
+++ lib/FriendListScreen.dart
@@ -1,19 +1,44 @@
-import 'package:chat_app/ChatScreen.dart';
 import 'package:chat_app/SearchScreen.dart';
+import 'package:chat_app/components/ChatTile.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class FriendListScreen extends StatefulWidget {
+class FriendListScreen extends StatelessWidget {
   const FriendListScreen({Key? key}) : super(key: key);
 
   @override
-  State<FriendListScreen> createState() => _FriendListScreenState();
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10.0),
+      child: FriendListStreamWidget(
+        firestore: FirebaseFirestore.instance,
+        userId: FirebaseAuth.instance.currentUser?.uid ?? '',
+      ),
+    );
+  }
 }
 
-class _FriendListScreenState extends State<FriendListScreen> {
-  User? user = FirebaseAuth.instance.currentUser;
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+class FriendListStreamWidget extends StatefulWidget {
+  final FirebaseFirestore firestore;
+  final String userId;
+
+  const FriendListStreamWidget({
+    Key? key,
+    required this.firestore,
+    required this.userId,
+  }) : super(key: key);
+
+  @override
+  _FriendListStreamWidgetState createState() => _FriendListStreamWidgetState();
+}
+
+class _FriendListStreamWidgetState extends State<FriendListStreamWidget> {
+  final ScrollController _scrollController = ScrollController();
+  final List<Map<String, dynamic>> _friendList = [];
+  int _itemsPerPage = 10;
+  int _currentMaxItems = 10;
+  bool _isLoading = false;
 
   @override
   void initState() {
@@ -21,149 +46,119 @@ class _FriendListScreenState extends State<FriendListScreen> {
   }
 
   @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 10.0),
-      child: StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-        stream: _firestore.collection('users').doc(user?.uid ?? '').snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (snapshot.hasError) {
-            return const Center(
-              child: Text('Something went wrong'),
-            );
-          } else if (snapshot.hasData) {
-            final data = snapshot.data?.data() as Map<String, dynamic>;
-            List<dynamic> friendList = data['friends'] ?? [];
+    return StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+      stream:
+          widget.firestore.collection('users').doc(widget.userId).snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
 
-            if (friendList.isEmpty) {
-              return Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('You don\'t have any friends yet',
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold)),
-                    SizedBox(height: 10),
-                    Text('Search for your friends to start chatting',
-                        style: TextStyle(fontSize: 16)),
-                    ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => SearchScreen()));
-                        },
-                        child: Text("Search"))
-                  ],
-                ),
-              );
-            }
+        if (snapshot.hasError) {
+          return const Center(child: Text('Something went wrong'));
+        }
 
-            friendList.sort((a, b) {
-              return b['lastMessage']['time'] - a['lastMessage']['time'];
-            });
+        final friendList =
+            (snapshot.data?.data()?['friends'] as List<dynamic>?) ?? [];
 
-            return ListView.builder(
-              itemCount: friendList.length,
-              itemBuilder: (context, index) {
-                final friendId = friendList[index]['uid'];
-                var _lastMessage = friendList[index]['lastMessage']['text'];
+        if (friendList.isEmpty) {
+          return _buildEmptyFriendList(context);
+        }
 
-                final lastMessage =
-                    _lastMessage is String ? _lastMessage : "Media file";
+        friendList.sort(
+            (a, b) => b['lastMessage']['time'] - a['lastMessage']['time']);
 
-                final isSeen =
-                    friendList[index]['lastMessage']['seen'] ?? false;
-
-                final showUnread = isSeen == false &&
-                    friendList[index]['lastMessage']['by'] != user?.uid;
-
-                return FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-                  future: _firestore.collection('users').doc(friendId).get(),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    } else if (snapshot.hasError) {
-                      return const Center(
-                        child: Text('Something went wrong'),
-                      );
-                    } else if (snapshot.hasData) {
-                      final friendData =
-                          snapshot.data?.data() as Map<String, dynamic>;
-                      final friendName = friendData['displayName'] as String;
-                      final avatar = friendData['image'] ?? '';
-
-                      return GestureDetector(
-                        onTap: () => Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) =>
-                                ChatScreen(partnerId: friendId),
-                          ),
-                        ),
-                        child: ListTile(
-                            leading: avatar == null || avatar == ""
-                                ? const CircleAvatar(
-                                    backgroundColor: Colors.grey,
-                                    child: Icon(Icons.person),
-                                  )
-                                : CircleAvatar(
-                                    backgroundImage:
-                                        Image.network(avatar).image,
-                                    maxRadius: 20,
-                                  ),
-                            title: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Row(
-                                  children: [
-                                    Text(
-                                      friendName,
-                                      style: const TextStyle(
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                    const SizedBox(width: 5),
-                                    Icon(
-                                      Icons.circle,
-                                      size: 10,
-                                      color: showUnread
-                                          ? Colors.green
-                                          : Colors.transparent,
-                                    )
-                                  ],
-                                ),
-                                SizedBox(height: 5),
-                                Text(
-                                  lastMessage,
-                                  style: const TextStyle(
-                                      fontWeight: FontWeight.normal,
-                                      fontSize: 12),
-                                )
-                              ],
-                            )),
-                      );
-                    } else {
-                      return const Center(
-                        child: Text('You don\'t have any friends!'),
-                      );
-                    }
-                  },
-                );
-              },
-            );
-          } else {
-            return const Center(
-              child: Text('You don\'t have any friends!'),
-            );
+        _friendList
+            .addAll(friendList.map((friend) => friend as Map<String, dynamic>));
+        List duplicateHelper = [];
+        _friendList.removeWhere((friend) {
+          if (duplicateHelper.contains(friend['uid'])) {
+            return true;
           }
-        },
+          duplicateHelper.add(friend['uid']);
+          return false;
+        });
+        return ListView.builder(
+          controller: _scrollController,
+          itemCount: _friendList.length - 1,
+          itemBuilder: (context, index) {
+            return _buildFriendTile(_friendList[index]);
+          },
+        );
+      },
+    );
+  }
+
+  Widget _buildEmptyFriendList(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'You don\'t have any friends yet',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 10),
+          const Text(
+            'Search for your friends to start chatting',
+            style: TextStyle(fontSize: 16),
+          ),
+          ElevatedButton(
+            onPressed: () => Navigator.push(
+                context, MaterialPageRoute(builder: (_) => SearchScreen())),
+            child: const Text("Search"),
+          ),
+        ],
       ),
+    );
+  }
+
+  Widget _buildFriendTile(Map<String, dynamic> friend) {
+    final friendId = friend['uid'];
+    String lastMessage;
+
+    if (friend['lastMessage']?['isImage'] != null) {
+      lastMessage = 'Media file';
+    } else {
+      // Check if 'lastMessage' is a Map and 'text' is a String
+      final lastMessageMap = friend['lastMessage'] as Map<String, dynamic>?;
+      if (lastMessageMap != null && lastMessageMap['text'] is String) {
+        lastMessage = lastMessageMap['text'] ?? "No Message";
+      } else {
+        lastMessage = 'No message';
+      }
+    }
+    final showUnread = friend['lastMessage']['seen'] == false &&
+        friend['lastMessage']['by'] != widget.userId;
+
+    return FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+      future: widget.firestore.collection('users').doc(friendId).get(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const SizedBox();
+        }
+
+        if (!snapshot.hasData) {
+          return const SizedBox();
+        }
+
+        final friendData = snapshot.data!.data()!;
+        // return Text('${friendData['displayName']} : $friendId');
+        return ChatTile(
+          friendId: friendId,
+          avatar: friendData['image'] ?? '',
+          friendName: friendData['displayName'],
+          showUnread: showUnread,
+          lastMessage: lastMessage,
+        );
+      },
     );
   }
 }
---
+++ lib/components/ChatTile.dart
@@ -0,0 +1,66 @@
+import 'package:chat_app/ChatScreen.dart';
+import 'package:flutter/material.dart';
+
+class ChatTile extends StatelessWidget {
+  const ChatTile({
+    super.key,
+    required this.friendId,
+    required this.avatar,
+    required this.friendName,
+    required this.showUnread,
+    required this.lastMessage,
+  });
+
+  final friendId;
+  final avatar;
+  final String friendName;
+  final bool showUnread;
+  final String lastMessage;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ChatScreen(partnerId: friendId),
+        ),
+      ),
+      child: ListTile(
+          leading: avatar == null || avatar == ""
+              ? const CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  child: Icon(Icons.person),
+                )
+              : CircleAvatar(
+                  backgroundImage: Image.network(avatar).image,
+                  maxRadius: 20,
+                ),
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text(
+                    friendName,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(width: 5),
+                  Icon(
+                    Icons.circle,
+                    size: 10,
+                    color: showUnread ? Colors.green : Colors.transparent,
+                  )
+                ],
+              ),
+              SizedBox(height: 5),
+              Text(
+                lastMessage,
+                style: const TextStyle(
+                    fontWeight: FontWeight.normal, fontSize: 12),
+              )
+            ],
+          )),
+    );
+  }
+}
